ccccb2b7bd4d959ecfc68a99dcf1cdeae77a81d9
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2022-06-26 10:54:00 +1100

    Stage 1

---
+++ main.kts
@@ -0,0 +1,52 @@
+class offBoundsError(message:String): Exception(message)
+
+fun main() {
+    println("Connect Four")
+
+    var players: List<String> = emptyList<String>()
+    for (n in listOf("First", "Second")) {
+        println("$n player's name:")
+        players += readln()
+    }
+
+    val (playerOne, playerTwo) =  players
+
+    var gameBoardSize: List<Int> = emptyList()
+    var validInput: Boolean = false
+
+    do {
+        println("Set the board dimensions (Rows x Columns)\nPress Enter for default (6 x 7)")
+        val entry: String = readln()
+        if (!entry.isEmpty()) {
+            try {
+                gameBoardSize = entry.split("x", ignoreCase = true).map { it.trim().toInt() }
+                var checks: List<Boolean> = emptyList()
+                for (rc in gameBoardSize) {
+                    checks += rc !in 5..9
+                }
+                val wrongEntry: String = when {
+                    checks[0] && checks[1] -> "both rows and columns"
+                    checks[0] -> "rows"
+                    checks[1] -> "columns"
+                    else -> "none"
+                }
+                if (wrongEntry != "none") {
+                    throw offBoundsError("Board $wrongEntry should be from 5 to 9")
+                }
+                validInput = true
+            } catch (e: Exception) {
+                when (e) {
+                    is offBoundsError -> println(e.message)
+                    else -> println("Invalid input")
+                }
+            }
+        } else {
+            gameBoardSize = listOf(6, 7)
+            break
+        }
+    } while (!validInput)
+
+
+    println("$playerOne VS $playerTwo")
+    println("${ gameBoardSize.joinToString(" X ") } board")
+}
